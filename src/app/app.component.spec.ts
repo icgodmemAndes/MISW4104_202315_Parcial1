d47db64f033572dc493ceff075d8d98ced1056d9
Jhon Edinson Muñoz Riaños <je.munozr1@uniandes.edu.co>
2023-11-03 21:04:26 -0500

    Implementa punto 3, adiciona prueba unitaria para componente coffee-list

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { CoffeeListComponent } from './coffee/coffee-list/coffee-list.component';
 
 describe('AppComponent', () => {
   beforeEach(() => TestBed.configureTestingModule({
-    imports: [RouterTestingModule],
-    declarations: [AppComponent]
+    imports: [RouterTestingModule, HttpClientModule],
+    declarations: [AppComponent, CoffeeListComponent]
   }));
 
   it('should create the app', () => {
@@ -20,10 +22,4 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('parcial');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('parcial app is running!');
-  });
 });
--- src/app/coffee/coffee-list/coffee-list.component.spec.ts
+++ src/app/coffee/coffee-list/coffee-list.component.spec.ts
@@ -2,16 +2,23 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
+import { faker } from '@faker-js/faker';
 
 import { CoffeeListComponent } from './coffee-list.component';
+import { HttpClientModule } from '@angular/common/http';
+import { Coffee } from '../coffee';
+import { CoffeeService } from '../coffee.service';
 
 describe('CoffeeListComponent', () => {
   let component: CoffeeListComponent;
   let fixture: ComponentFixture<CoffeeListComponent>;
+  let debug: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CoffeeListComponent ]
+      imports: [HttpClientModule],
+      declarations: [ CoffeeListComponent ],
+      providers: [CoffeeService]
     })
     .compileComponents();
   }));
@@ -19,10 +26,34 @@ describe('CoffeeListComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(CoffeeListComponent);
     component = fixture.componentInstance;
+
+    for (let i = 0; i < 3; i++) {
+      const coffee = new Coffee(
+        faker.number.int(),
+        faker.commerce.productName(),
+        faker.lorem.sentence(), 
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+        faker.number.int(),
+        faker.image.imageUrl(),
+      );
+      component.coffees.push(coffee);
+    }
+    
+
     fixture.detectChanges();
+    debug = fixture.debugElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  it('should have 3 <tr.fila> elements', () => {
+    expect(debug.queryAll(By.css('tr.fila'))).toHaveSize(3)
+  });
+
+  it('should have 3 elements in coffee list', () => {
+    expect(component.coffees).toHaveSize(3);
+  });
+ 
 });
